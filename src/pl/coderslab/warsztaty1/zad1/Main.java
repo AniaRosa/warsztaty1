a9f234d247ec50673b0415b71bc5728e4dcf9250
Anna <anrosa.j@gmail.com>
2019-11-09 12:58:54 +0100

    Zadanie1

---
+++ src/pl/coderslab/warsztaty1/zad1/Main.java
@@ -0,0 +1,46 @@
+package pl.coderslab.warsztaty1.zad1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Zadanie1");
+
+        Random r = new Random();
+        int wylosowanaLiczba = r.nextInt(100)+1;
+
+        System.out.println("Wylosowałem liczbę, zgadnij jaką?");
+
+        boolean win = false;
+        int l = 0;
+        while (!win) {
+            int liczba = wczytajLiczbe();
+            l++;
+
+            if (liczba < wylosowanaLiczba) {
+                System.out.println("Podana liczba jest za mała!");
+            } else if (liczba > wylosowanaLiczba) {
+                System.out.println("Podana liczba jest za duża!");
+            } else {
+                System.out.println("Zgadłeś za " + l + " razem");
+                win = true;
+            }
+        }
+    }
+
+    public static int wczytajLiczbe() {
+        System.out.println("Podaj liczbę");
+
+        Scanner scan = new Scanner(System.in);
+        while (!scan.hasNextInt()) {
+            System.out.println("Musisz podać liczbę!");
+            scan.next();
+        }
+
+        return scan.nextInt();
+    }
+}
